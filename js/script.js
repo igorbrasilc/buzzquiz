184f3db5a27bed4bb13b24c7c299ffb56394f00e
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-02-15 19:09:39 -0300

    layout de entrada pronto com inserção dinâmica, se não tiver quizz criado, abre autaticamente a tela de criar quiz

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let madeQuizzes = null;
+
+function checkIfMadeQuizzes() {
+    const containerMyQuizzes = document.querySelector(".my-quizzes");
+
+    if (madeQuizzes === null) {
+        containerMyQuizzes.innerHTML = `
+        <article class="create-first-quiz">
+            <p>Você não criou nenhum quizz ainda :(</p>
+            <p><span>Criar Quizz</span></p>
+        </article>
+        `;
+    } else {
+        containerMyQuizzes.innerHTML = `
+        <div class="created-quizzes-header">
+            <p>Seus Quizzes</p>
+            <ion-icon name="add-circle"></ion-icon>
+        </div>
+        <div class="created-quizzes">
+            <article class="created-quiz">
+                <img src="./img/homer-simpson.jpeg" alt="imagem-quiz"/>
+                <p><span>Quizz do Homer sdadasdsa dsad sa sad asd sad ad asd sa das</span></p>
+            </article>
+        </div>
+        `
+    }
+}
+
+checkIfMadeQuizzes();
+
