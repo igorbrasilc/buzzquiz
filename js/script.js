ceac20fbf30f1cd18ef6edc6e0d363549bdf5620
Paulo Uilian <uilianlbarros@gmail.com>
2022-02-16 08:17:03 -0300

    Implementará recebimento de quizz via objeto na tela de quiz

--- js/script.js
+++ js/script.js
@@ -1,3 +1,85 @@
+const QUIZ_TESTE = {
+    "id": 2254,
+    "title": "Quem é você na fila do pão",
+    "image": "https://miro.medium.com/max/1200/1*CT3u9Zejnbzdg36CI5zxDg.jpeg",
+    "questions": [
+      {
+        "title": "Título da pergunta 1",
+        "color": "#123456",
+        "answers": [
+          {
+            "text": "Texto da resposta 1",
+            "image": "https://http.cat/411.jpg",
+            "isCorrectAnswer": true
+          },
+          {
+            "text": "Texto da resposta 2",
+            "image": "https://http.cat/412.jpg",
+            "isCorrectAnswer": false
+          }
+        ]
+      },
+      {
+        "title": "Título da pergunta 2",
+        "color": "#123456",
+        "answers": [
+          {
+            "text": "Texto da resposta 1",
+            "image": "https://http.cat/411.jpg",
+            "isCorrectAnswer": true
+          },
+          {
+            "text": "Texto da resposta 2",
+            "image": "https://http.cat/412.jpg",
+            "isCorrectAnswer": false
+          }
+        ]
+      },
+      {
+        "title": "Título da pergunta 3",
+        "color": "#123456",
+        "answers": [
+          {
+            "text": "Texto da resposta 1",
+            "image": "https://http.cat/411.jpg",
+            "isCorrectAnswer": true
+          },
+          {
+            "text": "Texto da resposta 2",
+            "image": "https://http.cat/412.jpg",
+            "isCorrectAnswer": false
+          }
+        ]
+      }
+    ],
+    "levels": [
+      {
+        "title": "Título do nível 1",
+        "image": "https://http.cat/411.jpg",
+        "text": "Descrição do nível 1",
+        "minValue": 0
+      },
+      {
+        "title": "Título do nível 2",
+        "image": "https://http.cat/412.jpg",
+        "text": "Descrição do nível 2",
+        "minValue": 50
+      }
+    ]
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
 let madeQuizzes = null;
 
 function checkIfMadeQuizzes() {
@@ -64,4 +146,74 @@ function answerSelection(answer){
             console.log(pList[i]);
         }
     } 
+}
+
+
+function quizMake(){
+    const title = QUIZ_TESTE.title;
+    const imgHeader = QUIZ_TESTE.image;
+
+    let questions = QUIZ_TESTE.questions;
+    questions = shuffleArray(questions);
+
+    const documentImgHeader = document.querySelector('.quiz-page-header');
+    const documentTitle = document.querySelector('header h2');
+
+    documentTitle.innerHTML = title;
+
+    const quizPage = document.querySelector('.quiz-page');
+
+    for(let i = 0; i < questions.length; i++){
+        quizPage.innerHTML += questionMake(questions[i]);
+    }
+
+}
+
+function questionMake(question){
+    return `<article>
+        <header>${question.title}?</header>
+        <section>
+        ${answerMake(question.answers)}
+        </section>
+    </article>`;
+}
+
+function answerMake(answers){
+    let output = '';
+    for(let i = 0; (i + 1)  < answers.length; i+=2){
+      const img1 = answers[i].image;
+      const describe1 = answers[i].text;
+      const img2 = answers[i+1].image;
+      const describe2 = answers[i+1].text;
+      output += `
+            <div class="left">                    
+              <div onclick="answerSelection(this)">
+              <img src="${img1}" alt="">
+              <p>${describe1}</p>
+              <div class="white-cover"></div>
+              </div>
+            </div>
+            <div class="right">
+                <div onclick="answerSelection(this)">
+                <img src="${img2}" alt="">
+                <p>${describe2}</p>
+                <div class="white-cover"></div>
+                </div>                    
+            </div>`
+
+    }
+    return output;
+
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        // gera índices aleatórios dentro do permitido para o 'array'
+        const j = (Math.floor((Math.random() * 10)%array.length));
+
+        const aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+    }
+    return array;
 }
