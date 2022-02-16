ab882286c58d6c9f8738254e282b37b11266c67a
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-02-16 13:28:25 -0300

    renderizando os quizzes do servidor na tela de entrada

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let madeQuizzes = null;
 const MAIN = document.querySelector("main");
+let CREATEDQUIZOBJECT = {};
+const CONSTAPI = "https://mock-api.driven.com.br/api/v4/buzzquizz";
 
+// Checa ao abrir o site se já existem quizzes criados pelo usuário
 function checkIfMadeQuizzes() {
     const containerMyQuizzes = document.querySelector(".my-quizzes");
 
@@ -8,7 +11,7 @@ function checkIfMadeQuizzes() {
         containerMyQuizzes.innerHTML = `
         <article class="create-first-quiz">
             <p>Você não criou nenhum quizz ainda :(</p>
-            <p><span onclick="createQuiz()">Criar Quizz</span></p>
+            <p><span onclick="createQuizScreen1()">Criar Quizz</span></p>
         </article>
         `;
     } else {
@@ -29,6 +32,8 @@ function checkIfMadeQuizzes() {
 
 checkIfMadeQuizzes();
 
+// Exibição do quiz:
+
 function openQuiz() {
     console.log('clicou');
     const quizHeader = document.querySelector(".quiz-page-header");
@@ -39,8 +44,33 @@ function openQuiz() {
     quizHeader.classList.toggle("hide");
 }
 
+// Disponibilização dos quizzes do servidor ao entrar na tela inicial:
+const promiseGetQuizzes = axios.get(`${CONSTAPI}/quizzes`);
+promiseGetQuizzes.then(renderQuizzes);
+promiseGetQuizzes.catch(error => {
+    console.error(error.response);
+    alert("Não deu pra obter os quizzes, verifique o erro ou avise o devinho");
+})
 
-function createQuiz() {
+function renderQuizzes(response) {
+    const containerAllQuizzes = document.querySelector(".all-quizzes-container");
+    const data = response.data;
+
+    data.forEach(quiz => {
+        containerAllQuizzes.innerHTML += `
+        <article onclick="openQuiz()">
+            <div class="bg-gradient">
+                <img src="${quiz.image}" alt="imagem-quiz"/>
+                <p><span>${quiz.title}</span></p>
+            </div>
+        </article>
+        `;
+    })
+}
+
+
+//  Criação do quiz
+function createQuizScreen1() {
     const containerCreateFirst = document.querySelector(".create-first-quiz");
     const pageCreate = document.querySelector(".create-quiz-page");
 
@@ -49,15 +79,38 @@ function createQuiz() {
     
     pageCreate.innerHTML = `
     <h1>Comece pelo começo</h1>
-        <div class="container-questions">
-            <input type="text" placeholder="Título do seu quizz" class="title">
-            <input type="text" placeholder="URL da imagem do seu quizz" class="url">
-            <input type="text" placeholder="Quantidade de perguntas do quizz" class="question-qtd">
-            <input type="text" placeholder="Quantidade de níveis do quizz" class="level-qtd">
-        </div>
-        <button type="submit" class="btn-create">Prosseguir pra criar perguntas</button>
+    <div class="container-questions">
+        <input type="text" placeholder="Título do seu quizz" class="title">
+        <input type="url" placeholder="URL da imagem do seu quizz" class="url">
+        <input type="text" placeholder="Quantidade de perguntas do quizz" class="question-qtd">
+        <input type="text" placeholder="Quantidade de níveis do quizz" class="level-qtd">
+    </div>
+    <button type="submit" class="btn-create-screen-1" onclick="createQuizNextScreens(this)">Prosseguir pra criar perguntas</button>
     `;
 }
+
+// function createQuizNextScreens(btn) {
+//     const btnClass = btn.classList;
+//     const btnText = btn.innerHTML;
+//     const inputTitle = document.querySelector(".create-quiz-page .title").value;
+//     const inputURL = document.querySelector(".create-quiz-page .url").value;
+//     const inputQuestionQtd = document.querySelector(".create-quiz-page .question-qtd").value;
+//     const inputLevelQtd = document.querySelector(".create-quiz-page .level-qtd").value;
+
+//     CREATEDQUIZOBJECT = {
+//         title: inputTitle,
+//         URL: inputURL,
+//         questionQtd: inputQuestionQtd,
+//         levelQtd: inputLevelQtd
+//     }
+
+//     // Validations
+//     const titleOK = CREATEDQUIZOBJECT.title.length >= 20 && CREATEDQUIZOBJECT.title.length <= 65;
+
+//     if (btnClass.contains("btn-create-screen-1")) {
+//         console.log(CREATEDQUIZOBJECT);
+//     }
+// }
 
 
 /* Comportamento de respostas */
