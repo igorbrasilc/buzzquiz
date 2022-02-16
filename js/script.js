dbac8efb17fe423ecd4a201b9e8ad97bca9cfe19
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-02-16 09:11:15 -0300

    Com tela de perguntas basicas da criação do quiz funcionando

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let madeQuizzes = null;
+const MAIN = document.querySelector("main");
 
 function checkIfMadeQuizzes() {
     const containerMyQuizzes = document.querySelector(".my-quizzes");
@@ -7,7 +8,7 @@ function checkIfMadeQuizzes() {
         containerMyQuizzes.innerHTML = `
         <article class="create-first-quiz">
             <p>Você não criou nenhum quizz ainda :(</p>
-            <p><span>Criar Quizz</span></p>
+            <p><span onclick="createQuiz()">Criar Quizz</span></p>
         </article>
         `;
     } else {
@@ -30,10 +31,28 @@ checkIfMadeQuizzes();
 
 function openQuiz() {
     const quizHeader = document.querySelector(".quiz-page-header");
-    const main = document.querySelector("main");
     const quizPage = document.querySelector(".quiz-page");
 
-    main.classList.toggle("hide");
+    MAIN.classList.toggle("hide");
     quizPage.classList.toggle("hide");
     quizHeader.classList.toggle("hide");
+}
+
+function createQuiz() {
+    const containerCreateFirst = document.querySelector(".create-first-quiz");
+    const pageCreate = document.querySelector(".create-quiz-page");
+
+    MAIN.classList.toggle("hide");
+    containerCreateFirst.classList.toggle("hide");
+    
+    pageCreate.innerHTML = `
+    <h1>Comece pelo começo</h1>
+        <div class="container-questions">
+            <input type="text" placeholder="Título do seu quizz" class="title">
+            <input type="text" placeholder="URL da imagem do seu quizz" class="url">
+            <input type="text" placeholder="Quantidade de perguntas do quizz" class="question-qtd">
+            <input type="text" placeholder="Quantidade de níveis do quizz" class="level-qtd">
+        </div>
+        <button type="submit" class="btn-create">Prosseguir pra criar perguntas</button>
+    `;
 }
