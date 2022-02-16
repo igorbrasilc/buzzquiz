00412cd4032375e079a2b81bfce1d8dc60a83c0a
Paulo Uilian <uilianlbarros@gmail.com>
2022-02-15 22:01:55 -0300

    Implementará marcar resposta nas perguntas dos quiz

--- js/script.js
+++ js/script.js
@@ -28,3 +28,37 @@ function checkIfMadeQuizzes() {
 
 checkIfMadeQuizzes();
 
+
+
+
+
+
+
+
+
+
+
+/* Comportamento de respostas */
+function answerSelection(answer){
+    console.log('clicou');
+    const coverSelection = answer.querySelector('.white-cover');
+    const pSelection = answer.querySelector('p');
+
+    const section = answer.parentNode;
+    const coverList = section.querySelectorAll('.white-cover');
+    const pList = section.querySelectorAll('p');
+
+    for(let i = 0; i < coverList.length; i++){
+        if(coverList[i] !== coverSelection){
+            coverList[i].classList.add('success-quiz-answer-selection');
+            pList[i].classList.add('error-quiz-answer-selection');
+            pList[i].classList.remove('success-quiz-answer-selection');
+        }
+        else {
+            coverList[i].classList.remove('success-quiz-answer-selection');
+            pList[i].classList.remove('error-quiz-answer-selection');
+            pList[i].classList.add('success-quiz-answer-selection');
+            console.log(pList[i]);
+        }
+    } 
+}
